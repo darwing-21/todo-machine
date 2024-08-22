bb13e490ac2b0c67d0780f908c1c63849459c062
darwing-21 <darwingarcia2130@gmail.com>
2024-08-22 10:37:26 -0400

    feat(todo machine): add onClick and onChange

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,15 @@ import './TodoSearch.css';
 
 function TodoSearch() {
     return (
-        <input className="TodoSearch" placeholder="Cortar cebolla" />
+        <input
+            className="TodoSearch"
+            placeholder="Cortar cebolla"
+            onChange={(event) => {
+                console.log('Escribiste en el todo search')
+                console.log(event)
+                console.log(event.target)
+                console.log(event.target.value)
+            }} />
     );
 }
 
